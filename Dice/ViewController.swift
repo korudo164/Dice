e11b3da2f0e54aca191f46ebc553f5d0d37bfc58
Amu <korudo164@gmail.com>
2022-08-03 16:26:19 +0800

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by 阿姆 on 2022/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
